707225440edf7726d412176b1fa2a1f86ca66954
Rodrigo Pituba de Souza <rodrigopitubasouza@live.com>
2018-09-08 19:39:42 -0300

    Arrumando layout da página inicial(adicionando os contadores)e fazendo a conta de cadastros de todas as categorias

--- app/src/main/java/com/example/ice/evento/MainActivity.java
+++ app/src/main/java/com/example/ice/evento/MainActivity.java
@@ -18,7 +18,13 @@ public class MainActivity extends AppCompatActivity {
     private static final int REQUEST_EXTERNO = 3;
     public static final String EXTERNO_NOME = "Nome Externo";
     public static final String EXTERNO_EMAIL = "Email Externo";
+    private int contAluno = 0;
+    private int contServidor = 0;
+    private int contExterno = 0;
     private TextView txtMensagem;
+    private TextView txtAlunoCont;
+    private TextView txtServidorCont;
+    private TextView txtExternoCont;
     private Button btnAluno;
     private Button btnServidor;
     private Button btnExterno;
@@ -30,7 +36,12 @@ public class MainActivity extends AppCompatActivity {
         btnServidor = (Button) findViewById(R.id.btn_servidor);
         btnExterno = (Button) findViewById(R.id.btn_externo);
         txtMensagem = (TextView) findViewById(R.id.txt_mensagem);
-
+        txtAlunoCont = (TextView) findViewById(R.id.txt_aluno_cont);
+        txtServidorCont = (TextView) findViewById(R.id.txt_servidor_cont);
+        txtExternoCont = (TextView) findViewById(R.id.txt_externo_cont);
+        txtAlunoCont.setText("Aluno: " + contAluno);
+        txtServidorCont.setText("Servidor: " + contServidor);
+        txtExternoCont.setText("Externo: " + contExterno);
         btnAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ExternoActivity.class);
-                startActivityForResult(intent,REQUEST_SERVIDOR);
+                startActivityForResult(intent,REQUEST_EXTERNO);
             }
         });
 
@@ -63,18 +74,24 @@ public class MainActivity extends AppCompatActivity {
             String nome = bundleResultado.getString(MainActivity.ALUNO_NOME);
             String matricula = bundleResultado.getString(MainActivity.ALUNO_MATRICULA);
             txtMensagem.setText("Olá "+ nome+" de Matricula: " + matricula);
+            contAluno++;
+            txtAlunoCont.setText("Aluno: " + contAluno);
         }
         else if(requestCode == MainActivity.REQUEST_SERVIDOR && resultCode == Activity.RESULT_OK && data != null){
             Bundle bundleResultado = data.getExtras();
             String nome = bundleResultado.getString(MainActivity.SERVIDOR_NOME);
             String siape = bundleResultado.getString(MainActivity.SERVIDOR_SIAPE);
             txtMensagem.setText("Olá "+ nome+" de SIAPE: " + siape);
+            contServidor++;
+            txtServidorCont.setText("Servidor: " + contServidor);
         }
         else if(requestCode == MainActivity.REQUEST_EXTERNO && resultCode == Activity.RESULT_OK && data != null){
             Bundle bundleResultado = data.getExtras();
             String nome = bundleResultado.getString(MainActivity.EXTERNO_NOME);
             String email = bundleResultado.getString(MainActivity.EXTERNO_EMAIL);
             txtMensagem.setText("Olá "+ nome+" de EMAIL: " + email);
+            contExterno++;
+            txtExternoCont.setText("Externo: " + contExterno);
         }
     }
 }
